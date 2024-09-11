562ea7850d4be409e6f9620127002e11dcf23883
Corax73 <ramaz999@inbox.ru>
2024-09-11 23:49:21 +0400

    added encryption of authentication data for the request and saving the result in a file

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"bytes"
+	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -21,12 +26,13 @@ type Sender struct {
 	State
 	InputRequest, InputKey, InputDomain, Display *widget.Entry
 	ScrollContainer                              *container.Scroll
-	SendBtn                                      *widget.Button
+	SendBtn, SaveResultBtn                       *widget.Button
 	SelectMethod                                 *widget.Select
 }
 
 func main() {
 	app := app.New()
+	window := app.NewWindow("Crm sender")
 
 	btnExit := widget.NewButton("Exit", func() {
 		app.Quit()
@@ -38,19 +44,24 @@ func main() {
 		Display:      widget.NewEntry(),
 	}
 
+	sender.InputRequest.SetPlaceHolder("Enter the address bar for the request")
+	sender.InputKey.SetPlaceHolder("Enter key")
+	sender.InputDomain.SetPlaceHolder("Enter domain")
+
 	sender.SelectMethod = sender.GetSelectMethod()
 	sender.ScrollContainer = sender.GetScrollDisplay()
 	sender.SendBtn = sender.SendBtnHandler()
+	sender.SaveResultBtn = sender.SaveResultBtnHandler(window)
 
-	content := container.NewGridWithRows(2,
+	content := container.NewGridWithRows(3,
 		container.NewGridWithColumns(
 			2,
 			container.NewGridWithRows(5, sender.InputRequest, sender.InputKey, sender.InputDomain, sender.SelectMethod, sender.SendBtn),
 			container.NewGridWithRows(1, sender.ScrollContainer),
 		),
+		sender.SaveResultBtn,
 		btnExit,
 	)
-	window := app.NewWindow("Crm sender")
 	window.SetContent(content)
 	window.CenterOnScreen()
 	window.Resize(fyne.NewSize(500, 400))
@@ -74,7 +85,7 @@ func (sender *Sender) GetScrollDisplay() *container.Scroll {
 
 func (sender *Sender) SendBtnHandler() *widget.Button {
 	return widget.NewButton("Send", func() {
-		if sender.InputRequest.Text != "" && sender.Method != "" {
+		if sender.InputRequest.Text != "" && sender.Method != "" && sender.InputKey.Text != "" && sender.InputDomain.Text != "" {
 			sender.Display.SetText("")
 			resp, err := sender.SendByMethod()
 			if err == nil {
@@ -104,18 +115,37 @@ func (sender *Sender) SendByMethod() (*http.Response, error) {
 	var err error
 	switch sender.Method {
 	case "GET":
-		resp, err = http.Get(sender.InputRequest.Text)
+		str := sender.GetMD5Hash(sender.InputDomain.Text + strconv.FormatInt(time.Now().Unix(), 10) + sender.InputKey.Text)
+		resp, err = http.Get(sender.InputRequest.Text + "?domain=" + sender.InputDomain.Text + "&time=" + strconv.FormatInt(time.Now().Unix(), 10) + "&token=" + str)
 	default:
 		return resp, err
 	}
 	return resp, err
 }
 
-func (httpSender *Sender) showResp(data string) {
+func (sender *Sender) showResp(data string) {
 	var strBuilder strings.Builder
 	strBuilder.WriteString("[")
 	strBuilder.WriteString("{")
-	httpSender.Display.SetText(data)
+	sender.Display.SetText(data)
 	strBuilder.WriteString("}")
 	strBuilder.WriteString("]")
+}
+
+func (httpSender *Sender) GetMD5Hash(text string) string {
+	hash := md5.Sum([]byte(text))
+	return hex.EncodeToString(hash[:])
+}
+
+func (sender *Sender) SaveResultBtnHandler(appWindow fyne.Window) *widget.Button {
+	return widget.NewButton("Save result to file", func() {
+		dialog.ShowFileSave(func(writer fyne.URIWriteCloser, err error) {
+			if err == nil && writer != nil {
+				_, err := writer.Write([]byte(sender.Display.Text))
+				if err != nil {
+					dialog.ShowError(err, appWindow)
+				}
+			}
+		}, appWindow)
+	})
 }
